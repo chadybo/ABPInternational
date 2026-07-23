75774d4dd56bdf4c3ed62b2a16bd9cf95796b3c0
aditya pradeep <adi112415@yahoo.com>
2026-07-23 14:49:13 -0400

    Inserted body statement in defaultLayout and fixed path resolution

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 import express from "express";
 import configRoutes from "./routes/index.js";
 import exphbs from "express-handlebars";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use("/public", express.static("public"));
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
 
 configRoutes(app);
 
